90037a3f09ce902218695e6710e19e8bf877ef40
Abd Elmalik Abd Elghafar <paydaychains100@gmail.com>
2023-05-02 06:20:45 +0300

    adding challenge 1 files

--- dist/assets/Loading-6c947985.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o,c,p as t,a,b as _}from"./index-88edc580.js";const n={},d=e=>(t("data-v-97de63a5"),e=e(),a(),e),i={class:"loading position-fixed d-flex justify-content-center align-items-center w-100 h-100"},p=d(()=>_("span",{class:"circle"},null,-1)),r=[p];function l(e,f){return o(),c("div",i,r)}const u=s(n,[["render",l],["__scopeId","data-v-97de63a5"]]);export{u as L};
