0681f0fc1072f087fca7c105bde2e6e11781e0de
Abd Elmalik Abd Elghafar <paydaychains100@gmail.com>
2023-05-02 06:21:32 +0300

    deploying

---
+++ dist/assets/Loading-94ecbac0.js
@@ -0,0 +1,1 @@
+import{_ as s,o,c,p as t,a,b as _}from"./index-62b2c0be.js";const n={},d=e=>(t("data-v-97de63a5"),e=e(),a(),e),i={class:"loading position-fixed d-flex justify-content-center align-items-center w-100 h-100"},p=d(()=>_("span",{class:"circle"},null,-1)),r=[p];function l(e,f){return o(),c("div",i,r)}const u=s(n,[["render",l],["__scopeId","data-v-97de63a5"]]);export{u as L};
