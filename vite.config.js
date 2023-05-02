1fe521863eec3cd693e09b4efcc1f4b6e2dd9e30
Abd Elmalik Abd Elghafar <paydaychains100@gmail.com>
2023-05-02 12:58:56 +0300

    deploying

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/REST-Countries-API-with-color-theme-switcher/',
-  plugins: [vue()],
-})
